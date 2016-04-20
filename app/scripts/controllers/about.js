93c835262158ada26eac2aeece330dd6e741918b
Lukasz <lukasz.ogan@gmail.com>
2016-04-20 12:52:12 +0200

    App

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allegroapiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the allegroapiApp
+ */
+angular.module('allegroapiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allegroapiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the allegroapiApp
+ */
+angular.module('allegroapiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
